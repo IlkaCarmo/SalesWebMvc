8578cfcd144cea17a7189532e0486c8e3aeaf77c
ilka <ilka_2009@ig.com.br git config --global user.email ilka_2009@ig.com.br>
2020-06-08 22:16:53 -0300

    Delete seller

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -44,5 +44,30 @@ namespace SalesWebMvc.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        public IActionResult Delete(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+
+            if(obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _sellerService.Remove(id);
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -27,6 +27,19 @@ namespace SalesWebMvc.Services
             _context.SaveChanges();
         }
 
+        public Seller FindById (int id)
+        {
+            return _context.Seller.FirstOrDefault(obj => obj.ID == id);
+        }
+
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
+            _context.SaveChanges();
+
+        }
+
 
     }
 }
